b7a0a9897431918d58f08988c2b7da904812dc0c
cpacheco-perello <cpacheco.perello@outlook.com>
2026-02-05 11:23:26 +0100

    corregido titulo de la página de búsqueda al catalán

--- Patch_Interface/agents/consumer/atro-config/astro/catalogue-ui/index.astro.mjs
+++ Patch_Interface/agents/consumer/atro-config/astro/catalogue-ui/index.astro.mjs
@@ -9,7 +9,7 @@ const $$Index = createComponent(($$result, $$props, $$slots) => {
   const Astro2 = $$result.createAstro($$Astro, $$props, $$slots);
   Astro2.self = $$Index;
   const search = Astro2.url.searchParams.get("search") || "";
-  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Busca en el catalogo federado" }, { "default": ($$result2) => renderTemplate` ${renderComponent($$result2, "PageHeader", PageHeader, { "title": "Busca en el catalogo federado" })} ${renderComponent($$result2, "SearchTabs", null, { "client:only": "vue", "search": search, "client:component-hydration": "only", "client:component-path": "/app/src/components/search/SearchTabs.vue", "client:component-export": "default" })} ` })}`;
+  return renderTemplate`${renderComponent($$result, "Layout", $$Layout, { "title": "Cerca al catàleg ESDATIB" }, { "default": ($$result2) => renderTemplate` ${renderComponent($$result2, "PageHeader", PageHeader, { "title": "Cerca al catàleg ESDATIB" })} ${renderComponent($$result2, "SearchTabs", null, { "client:only": "vue", "search": search, "client:component-hydration": "only", "client:component-path": "/app/src/components/search/SearchTabs.vue", "client:component-export": "default" })} ` })}`;
 }, "/app/src/pages/index.astro", void 0);
 
 const $$file = "/app/src/pages/index.astro";
